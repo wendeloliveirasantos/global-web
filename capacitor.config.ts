eb3ab09c3f4598434f6b3ed9315a113336952b0a
Wendel de Oliveira Santos <wendel.oliveira.santos@outlook.com>
2024-10-12 17:02:11 -0300

    android e ios

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.example.app',
-  appName: 'global-web',
+  appName: 'dobyseg',
   webDir: 'out',
   server: {
     androidScheme: 'https'
